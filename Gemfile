ec4d4ac37c1c61970645be22072ab0a63cdca6a5
Kent T <rinkira7@gmail.com>
2017-12-18 15:03:31 -0500

    clean up code

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 # Use Bootstrap for CSS framework
 gem 'bootstrap-sass', '~> 3.3.7'
+# For Save Game Upload
+gem "paperclip", '~> 5.0.0'
 
 group :production do
    gem 'pg'
--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -3,57 +3,8 @@ class ToolsController < ApplicationController
   end
 
   def mines
-  	@elevator = []
-  	@override = []
-  	@monster = []
-  	@slime = []
-  	@mushroom = []
-
+    @year = 1
   	# # Temporary Numbers for testing
   	@game_id = 123456789
-  	@day = 1
-
-  	# #Elevators every 5 levels
-  	# (6..119).each do |level|
-  	# 	if level % 5 == 0 
-  	# 		@elevator << level
-  	# 	elsif !(level % 40 > 5 && level % 40 < 30 && level % 40 != 19)
-  	# 		@override << level
-  	# 	end
-
-  	# 	# Infestation Checks
-  	# 	unless @elevator.include? level
-  	# 		unless @override.include? level
-		 #  		rng = CsRandom.new(day + level + id / 2)
-		 #  		if rng.next_double < 0.05
-		 #  			if rng.next_double < 0.5
-		 #  				@monster << level
-		 #  			else
-		 #  				@slime << level
-		 #  			end
-		 #  		end
-		 #  	end
-
-	  # 		# Mushroom Level Check. Reset RNG
-	  # 		if level > 80
-	  # 			rng = CsRandom.new(day + level + id / 2)
-
-	  # 			# Game uses rng for lighting checks
-	  # 			if rng.next_double < 0.3
-	  # 				rng.next_double
-	  # 			end
-	  # 			rng.next_double
-
-	  # 			if rng.next_double < 0.035
-	  # 				@mushroom << level
-	  # 			end
-
-	  # 			#
-	  # 		end
-	  # 	end
-
-
-  	# end
-  	
   end
 end
